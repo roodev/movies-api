4d78d7e926ccc33d3259f1a4ccb96ee5bd597591
roodev <rodrigo.fessore@gmail.com>
2020-08-21 01:25:28 -0300

    Disponibilizando rota para criar um filme

---
+++ SRC/app/routes/filmes.routes.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const route = express.Router()
+const Filme = require('./../controllers/filmes.controller')
+
+route.post('criar', Filme.criarFilme )
+
+module.exports = route
