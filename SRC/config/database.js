883438384f23c11b84c2aabcccf74a06fe21a48a
roodev <rodrigo.fessore@gmail.com>
2020-08-20 23:57:25 -0300

    correção

--- SRC/config/database.js
+++ SRC/config/database.js
@@ -6,10 +6,10 @@ let db = null
 /**Montando a URI do banco */
 const URI_DATABASE = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
 
-/**Criando conexão para outros módulos */
+/**Criando conexão com o banco de dados */
 db = mongoose.connect(URI_DATABASE, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => console.log('Banco de dados conectado com sucesso!'))
     .catch(error => console.log (`Problema ao conectar no banco de dados = ERRO: ${error}`))
 
-/** Exportando conexão para outros */
+/** Exportando conexão para outros módulos */
 module.exports = { db }
